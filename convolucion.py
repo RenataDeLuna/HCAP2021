09c0d85a048ed669e06a852a5f87a087497e6498
RenataDeLuna <a01750484@itesm.mx>
2021-03-19 08:34:27 -0600

    Nuevos archivos

--- convolucion.py
+++ convolucion.py
@@ -1,9 +1,9 @@
 import numpy as np
-
+import cv2
 def convolucion(Ioriginal,Kernel):
     fr = len(Ioriginal) - (len(Kernel)-1)
     cr = len(Ioriginal[0]) - (len(Kernel[0])-1)
-    Resultado = np.zeros((fr, cr))
+    Resultado = np.zeros((fr, cr),np.uint8)
     #For para recorrer las filas
     for i in range(len(Resultado)):
         #For para recorrer las columnas
@@ -13,7 +13,8 @@ def convolucion(Ioriginal,Kernel):
             for m in range(len(Kernel)):
                 for n in range(len(Kernel[0])):
                     suma += Kernel[m][n] * Ioriginal[m+i][n+j]
-            Resultado[i][j] = suma
+            if suma<=255:
+                Resultado[i][j] =round(suma)
     return Resultado
 
 #Generar artificialemente imágenes 
@@ -24,7 +25,13 @@ I = [[2,0,1,1,1],[3,0,0,0,2],[1,1,1,1,1],[3,1,1,1,2],[1,1,1,1,1]]
 In = np.array(I) 
 Kn = np.array(K)
 
+IRGB=cv2.imread('003.jpg')
+IGS=cv2.cvtColor(IRGB,cv2.COLOR_BGR2GRAY)
+print(IGS.shape)
+
 #Mandar llamar a la función convolución
-R = convolucion(In, Kn)
+R = convolucion(IGS, Kn)
 
 print(R)
+print(R.shape)
+cv2.imwrite('004C.jpg',R)
